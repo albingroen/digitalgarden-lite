4105af49b782a8e18cdaf50ccd7559bd263e11bd
albingroen <albin.groen@gmail.com>
2021-05-04 10:53:07 +0200

    :sparkles: Add read time

--- components/Logo/Logo.tsx
+++ components/Logo/Logo.tsx
@@ -5,8 +5,8 @@ export default function Logo() {
     <Link href="/">
       <img
         src="https://res.cloudinary.com/albin-groen/image/upload/f_auto,q_auto,w_100/v1602935502/logo_uws10e.svg"
+        className="w-10 cursor-pointer"
         alt="Albin Groen logo"
-        className="w-10"
         id="logo"
       />
     </Link>
--- pages/posts/[slug].tsx
+++ pages/posts/[slug].tsx
@@ -2,6 +2,7 @@ import matter from "gray-matter";
 import hydrate from "next-mdx-remote/hydrate";
 import renderToString from "next-mdx-remote/render-to-string";
 import Link from "next/link";
+import { useEffect, useRef } from "react";
 import Logo from "../../components/Logo/Logo";
 import PostSeo from "../../components/PostSeo/PostSeo";
 import { getAllPostSlugs, getPostdata } from "../../lib/posts";
@@ -10,6 +11,8 @@ const components = {};
 
 export default function Posts({ source, frontMatter }) {
   const content = hydrate(source, { components });
+
+  const articleContent = useRef<HTMLDivElement>(null);
 
   return (
     <>
@@ -22,13 +25,29 @@ export default function Posts({ source, frontMatter }) {
       <div className="max-w-screen-lg mx-auto py-12 px-8">
         <Logo />
 
-        <Link href="/">
-          <span className="mt-12 inline-block text-blue-500 underline cursor-pointer">
-            Back to all posts
-          </span>
-        </Link>
+        <div className="flex items-center justify-between">
+          <Link href="/">
+            <span className="mt-12 inline-block text-blue-500 underline cursor-pointer">
+              Back to all posts
+            </span>
+          </Link>
 
-        <article className="prose prose-blue prose-lg mx-auto max-w-screen-lg mt-12">
+          {articleContent.current && (
+            <span className="uppercase text-gray-500 tracking-wide font-semibold font-mono text-sm">
+              Read time: ~
+              {Math.round(
+                articleContent.current.textContent.trim().split(" ").length /
+                  200
+              )}{" "}
+              minutes
+            </span>
+          )}
+        </div>
+
+        <article
+          ref={articleContent}
+          className="prose prose-blue prose-lg mx-auto max-w-screen-lg mt-12"
+        >
           <header>
             <h1>{frontMatter.title}</h1>
             <p>{frontMatter.excerpt}</p>
