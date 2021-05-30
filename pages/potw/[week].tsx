51fc2f09f3b22029b401229968f3bf9966f54032
Albin Groen <albin.groen@gmail.com>
2021-05-30 09:03:14 +0200

    :recycle: Remove unused import

--- pages/potw/[week].tsx
+++ pages/potw/[week].tsx
@@ -1,5 +1,4 @@
 import Head from "next/head";
-import { useRouter } from "next/router";
 import allProducts from "../../potw.json";
 import SeoImage from "../../components/SeoImage/SeoImage";
 
@@ -32,21 +31,21 @@ function Potw({ week, image, products }) {
         <SeoImage source={image} />
       </Head>
 
-      <div className="max-w-screen-lg mx-auto py-12 px-0 sm:px-8">
+      <div className="px-0 py-12 mx-auto max-w-screen-lg sm:px-8">
         <header className="px-5 sm:px-0">
-          <h1 className="font-semibold sm:font-medium text-2xl sm:text-3xl">
+          <h1 className="text-2xl font-semibold sm:font-medium sm:text-3xl">
             {title}
           </h1>
-          <p className="text-gray-500 mt-1 sm:mt-3 text-sm sm:text-base">
+          <p className="mt-1 text-sm text-gray-500 sm:mt-3 sm:text-base">
             {description}
           </p>
         </header>
 
         {products.length ? (
-          <div className="w-full flex flex-col divide-y rounded-none sm:rounded-md border shadow-sm bg-white mt-5 sm:mt-8">
-            {products.map((product, i) => (
+          <div className="flex flex-col w-full mt-5 bg-white border rounded-none divide-y sm:rounded-md shadow-sm sm:mt-8">
+            {products.map((product) => (
               <a
-                className="py-3 px-5 sm:p-4 flex items-center justify-between cursor-pointer group transition hover:bg-blue-50 focus:outline-none focus:ring-2"
+                className="flex items-center justify-between px-5 py-3 cursor-pointer sm:p-4 group transition hover:bg-blue-50 focus:outline-none focus:ring-2"
                 href={product.link}
                 key={product.name}
                 target="_blank"
@@ -64,16 +63,16 @@ function Potw({ week, image, products }) {
                   />
 
                   <div>
-                    <h2 className="font-medium text-sm sm:text-base">
+                    <h2 className="text-sm font-medium sm:text-base">
                       <span>{i + 1}.</span> <span>{product.name}</span>
                     </h2>
-                    <p className="text-xs sm:text-sm mt-0 sm:mt-px text-gray-500">
+                    <p className="mt-0 text-xs text-gray-500 sm:text-sm sm:mt-px">
                       {product.description}
                     </p>
                   </div>
                 </div>
 
-                <span className="text-blue-500 transition-none group-hover:text-blue-600 text-lg sm:text-sm">
+                <span className="text-lg text-blue-500 transition-none group-hover:text-blue-600 sm:text-sm">
                   <span className="hidden sm:inline">Go to product </span>
                   &rarr;
                 </span>
@@ -81,7 +80,7 @@ function Potw({ week, image, products }) {
             ))}
           </div>
         ) : (
-          <p className="py-3 px-5 sm:p-4 text-gray-400 rounded-none  sm:rounded-md border shadow-sm bg-white mt-5 sm:mt-8 text-sm sm:text-base">
+          <p className="px-5 py-3 mt-5 text-sm text-gray-400 bg-white border rounded-none sm:p-4 sm:rounded-md shadow-sm sm:mt-8 sm:text-base">
             There are no products listed for this week yet...
           </p>
         )}
