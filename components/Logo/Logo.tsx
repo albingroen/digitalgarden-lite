cf54709dcce29836049490f66a57710495767d81
Albin Groen <albin.groen@gmail.com>
2021-06-15 17:04:10 +0200

    :recycle: More native links

--- components/Logo/Logo.tsx
+++ components/Logo/Logo.tsx
@@ -1,14 +1,12 @@
-import Link from "next/link";
-
 export default function Logo() {
   return (
-    <Link href="/">
+    <a href="/">
       <img
         src="https://res.cloudinary.com/albin-groen/image/upload/f_auto,q_auto,w_100/v1602935502/logo_uws10e.svg"
         className="w-10 cursor-pointer"
         alt="Albin Groen logo"
         id="logo"
       />
-    </Link>
+    </a>
   );
 }
