f0b02dde5960e27e2c02bd063a8eff91ab91989b
Albin Groen <albin.groen@gmail.com>
2021-06-22 22:42:24 +0200

    :lipstick: Update code block component

--- components/CodeBlock/CodeBlock.tsx
+++ components/CodeBlock/CodeBlock.tsx
@@ -7,26 +7,41 @@ const CodeBlock = ({ children, className }) => {
     : "javascript";
 
   return (
-    <Highlight
-      {...defaultProps}
-      code={children}
-      language={language}
-      theme={Dracula}
-    >
-      {({ className, style, tokens, getLineProps, getTokenProps }) => (
-        <pre className={className} style={{ ...style, padding: "20px" }}>
-          {tokens
-            .filter((_, i) => i !== tokens.length - 1)
-            .map((line, i) => (
-              <div key={i} {...getLineProps({ line, key: i })}>
-                {line.map((token, key) => (
-                  <span key={key} {...getTokenProps({ token, key })} />
-                ))}
-              </div>
-            ))}
-        </pre>
-      )}
-    </Highlight>
+    <div className="border-gray-700">
+      <div className="p-4 text-base font-medium leading-none tracking-wide text-gray-400 capitalize bg-gray-700 border border-gray-600 rounded-b-none rounded-md">
+        {language}
+      </div>
+
+      <Highlight
+        {...defaultProps}
+        code={children}
+        language={language}
+        theme={Dracula}
+      >
+        {({ style, tokens, getLineProps, getTokenProps }) => (
+          <pre
+            className="border-b border-l border-r border-gray-600"
+            style={{
+              ...style,
+              borderTopRightRadius: 0,
+              borderTopLeftRadius: 0,
+              padding: "20px",
+              margin: 0,
+            }}
+          >
+            {tokens
+              .filter((_, i) => i !== tokens.length - 1)
+              .map((line, i) => (
+                <div key={i} {...getLineProps({ line, key: i })}>
+                  {line.map((token, key) => (
+                    <span key={key} {...getTokenProps({ token, key })} />
+                  ))}
+                </div>
+              ))}
+          </pre>
+        )}
+      </Highlight>
+    </div>
   );
 };
 
