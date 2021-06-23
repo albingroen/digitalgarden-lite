52a05ac0c2da20428e27d02b9ded85acd892cc86
Albin Groen <albin.groen@gmail.com>
2021-06-23 17:49:25 +0200

    :sparkles: Add image zooming

--- pages/_app.js
+++ pages/_app.js
@@ -2,10 +2,17 @@ import "../styles/globals.css";
 import Head from "next/head";
 import { MDXProvider } from "@mdx-js/react";
 import CodeBlock from "../components/CodeBlock/CodeBlock";
+import ImageZoom from "react-medium-image-zoom";
+import "react-medium-image-zoom/dist/styles.css";
 
 const components = {
   pre: (props) => <div {...props} />,
   code: CodeBlock,
+  img: (props) => (
+    <ImageZoom>
+      <img {...props} style={{ margin: 0 }} />
+    </ImageZoom>
+  ),
 };
 
 function MyApp({ Component, pageProps }) {
